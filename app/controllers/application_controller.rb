5b480d8edb725c65fa60ff5ac4e7e653b655f316
Krisetn Nestler <kristen.nestler@gmail.com>
2022-11-16 02:18:35 -0500

    remove comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,3 @@
 class ApplicationController < ActionController::Base
-  # before_action :github_info
 
-  # def github_info
-  #   @repo = GithubSearch.repo_name
-  #   @commits = GithubSearch.commits
-  #   @pulls = GithubSearch.pulls
-  # end
 end
